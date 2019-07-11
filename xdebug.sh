c09d2995e7f858970b9c994c02602c86568648bd
Ivan <win32.ya@gmail.com>
2019-07-11 12:28:41 +0500

    Clean-up xdebug

--- xdebug.sh
+++ xdebug.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 if [ $EUID -ne 0 ]; then
-  >&2 echo -e '\e[91mPlease run as root.\e[0m'
+  >&2 ansi --red --bold 'This script should be run as root.'
   exit 1
 fi
 
-if [ $1 = 'on' ]; then
-  phpenmod -s fpm xdebug && phpenmod -s cli xdebug && service php%PHP_VERSION%-fpm reload
-elif [ $1 = 'off' ]; then
-  phpdismod -s fpm xdebug && phpdismod -s cli xdebug && service php%PHP_VERSION%-fpm reload
+if [ "$1" = 'on' ]; then
+  phpenmod xdebug && service apache2 reload
+elif [ "$1" = 'off' ]; then
+  phpdismod xdebug && service apache2 reload
 else
   >&2 echo Usage: $(basename -- "$0") '[on|off]';
   exit 1
