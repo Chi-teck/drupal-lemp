b503c9ad11eeb8938bf693bbf145c1268eee7f5d
Ivan <Chi-teck@users.noreply.github.com>
2019-08-13 19:45:07 +0500

    Update xdebug.sh

--- xdebug.sh
+++ xdebug.sh
@@ -5,9 +5,9 @@ if [ $EUID -ne 0 ]; then
   exit 1
 fi
 
-if [ $1 = 'on' ]; then
+if [ "$1" = 'on' ]; then
   phpenmod -s fpm xdebug && phpenmod -s cli xdebug && service php%PHP_VERSION%-fpm reload
-elif [ $1 = 'off' ]; then
+elif [ "$1" = 'off' ]; then
   phpdismod -s fpm xdebug && phpdismod -s cli xdebug && service php%PHP_VERSION%-fpm reload
 else
   >&2 echo Usage: $(basename -- "$0") '[on|off]';
