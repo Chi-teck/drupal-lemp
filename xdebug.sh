658ee64f9f535d85cec01478182a42007a66e53c
Ivan <win32.ya@gmail.com>
2019-08-13 12:07:49 +0500

    Fix xdebug manager

--- xdebug.sh
+++ xdebug.sh
@@ -5,10 +5,10 @@ if [ $EUID -ne 0 ]; then
   exit 1
 fi
 
-if [ "$1" = 'on' ]; then
-  phpenmod xdebug && service apache2 reload
-elif [ "$1" = 'off' ]; then
-  phpdismod xdebug && service apache2 reload
+if [ $1 = 'on' ]; then
+  phpenmod -s fpm xdebug && phpenmod -s cli xdebug && service php%PHP_VERSION%-fpm reload
+elif [ $1 = 'off' ]; then
+  phpdismod -s fpm xdebug && phpdismod -s cli xdebug && service php%PHP_VERSION%-fpm reload
 else
   >&2 echo Usage: $(basename -- "$0") '[on|off]';
   exit 1
